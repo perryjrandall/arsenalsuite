d10a478d71812d8c6fda33c4ee83e248e93a19f4
Aren Villanueva <yomogi.aren@gmail.com>
2015-06-03 17:51:48 +1000

    Fixing a few libfreezer dependencies.

--- cpp/lib/freezer/build.py
+++ cpp/lib/freezer/build.py
@@ -3,6 +3,12 @@ import os
 from blur.build import *
 
 path = os.path.dirname(os.path.abspath(__file__))
+sippath = os.path.join(path,'sipFreezer')
+
+try:
+    os.mkdir(sippath)
+except: pass
+
 rev_path = os.path.join(path,'../..')
 
 svn = WCRevTarget("libfreezersvnrev",path,rev_path,"include/svnrev-template.h","include/svnrev.h")
@@ -14,7 +20,7 @@ pc.pre_deps = ["libfreezer","pyclasses:install","pyabsubmit"]
 pcs = SipTarget("pyfreezerstatic",path,True)
 pcs.pre_deps = ["libfreezer","pyclassesstatic:install","pyabsubmitstatic"]
 
-QMakeTarget("libfreezer",path,"libfreezer.pro",["classes","classesui",svn])
+QMakeTarget("libfreezer",path,"libfreezer.pro",["classes","classesui","libabsubmit",svn])
 #QMakeTarget("libassfreezerstatic",path,"libassfreezer.pro",["stonestatic","stoneguistatic","classesuistatic","libabsubmit"],[],True)
 
 if sys.platform=="linux2":
